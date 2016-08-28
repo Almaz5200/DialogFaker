9c972f3b9884b8d9c318f81b734a5025d0c7a206
Артем Трубачеев <trubacheev2013@ya.ru>
2016-08-28 23:09:41 +0800

    feat: added VK style messages VC

--- DialogFaker/MesPickViewController.swift
+++ DialogFaker/MesPickViewController.swift
@@ -7,11 +7,47 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class MesPickViewController: UIViewController {
+class MesPickViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var typePick: UISegmentedControl!
+    @IBOutlet weak var namePick: UITextField!
+    @IBOutlet weak var avatarPick: UIButton!
+    @IBOutlet weak var createDialog: UIButton!
+    
+    var opponentAvatar: UIImage?
+    
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        avatarPick.layer.cornerRadius = 10
+        avatarPick.clipsToBounds = true
+        avatarPick.rx_tap
+            .subscribeNext { _ in
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = .PhotoLibrary
+                self.presentViewController(imagePicker, animated: true, completion: nil)
+            }
+            .addDisposableTo(disposeBag)
+        
+        createDialog.rx_tap
+            .subscribeNext { _ in
+                let messagesVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("MessagesViewController") as! MessagesViewController
+                messagesVC.opponentAvatar = self.opponentAvatar
+                messagesVC.opponentName = self.namePick.text
+                
+                messagesVC.senderId = "sender"
+                messagesVC.senderDisplayName = "sender"
+                
+                self.navigationController?.showViewController(messagesVC, sender: self)
+            }
+            .addDisposableTo(disposeBag)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +55,12 @@ class MesPickViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.opponentAvatar = image
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
--- DialogFaker/MessagesViewController.swift
+++ DialogFaker/MessagesViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import JSQMessagesViewController
+import RxSwift
 
-class MessagesViewController: UIViewController {
-
+class MessagesViewController: JSQMessagesViewController {
+    
+    var messages = [JSQMessage]()
+    var outgoingBubbleImageView: JSQMessagesBubbleImage!
+    var incomingBubbleImageView: JSQMessagesBubbleImage!
+    
+    var opponentName: String?
+    var opponentAvatar: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.navigationController?.navigationBar.topItem?.title = ""
+        
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 87/255, green: 130/255, blue: 178/255, alpha: 1)
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        
+        collectionView.backgroundColor = UIColor(red: 237/255, green: 243/255, blue: 250/255, alpha: 1)
+        
+        setupBubbles()
+        
+        messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, text: "Hello"))
+        messages.append(JSQMessage(senderId: "as", displayName: "as", text: "Hello1"))
+        
+        finishReceivingMessage()
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +45,51 @@ class MessagesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
+    private func setupBubbles() {
+        let factory = JSQMessagesBubbleImageFactory()
+        outgoingBubbleImageView = factory.outgoingMessagesBubbleImageWithColor(UIColor(red: 205/255, green: 226/255, blue: 250/255, alpha: 1))
+        incomingBubbleImageView = factory.incomingMessagesBubbleImageWithColor(UIColor(red: 1, green: 1, blue: 1, alpha: 1))
+    }
+    
+    override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
+        
+    }
+    
+    override func didPressAccessoryButton(sender: UIButton!) {}
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAtIndexPath: indexPath) as! JSQMessagesCollectionViewCell
+        
+        cell.textView!.textColor = UIColor.blackColor()
+        
+        return cell
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item] // 1
+        if message.senderId == senderId { // 2
+            return outgoingBubbleImageView
+        } else { // 3
+            return incomingBubbleImageView
+        }
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
     /*
     // MARK: - Navigation
 
